181a69418335e73b7e491001dd2c10b939a16725
Taylor Barnett <taylormbarnett@utexas.edu>
2019-07-18 15:58:36 -0500

    upgrade from spectral v2 to v4

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const fs = require('fs')
 const yaml = require('js-yaml')
 
 const { Spectral } = require("@stoplight/spectral")
-const { defaultRuleset } = require("@stoplight/spectral/lib/rulesets")
-
+const { oas2Functions, rules: oas2Rules } = require('@stoplight/spectral/dist/rulesets/oas2')
 
 module.exports = (app) => {
   app.log('Yay! The app was loaded!')
   
   app.on(['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize'], async context => {
-    // remember that the config.yml file is the repo you are running the bot in
+    // remember: the config.yml file is the repo you are running the bot in
     const config = await context.config('config.yml')
     
     // currently, can only target one file
@@ -30,24 +29,27 @@ module.exports = (app) => {
      }
     
     // create a new instance of spectral with all of the baked in rulesets
-    const spectral = new Spectral({ rulesets: [defaultRuleset()] })
-
-    // run!
-    const results = spectral.run({ spec: 'oas2', target: content })
-
-    context.github.issues.createComment(context.issue({ body: formatLintResults(results)}))
+    const spectral = new Spectral()
+    
+    // leaving as OAS v2 for now, 
+    // in Spectral 4.1 the JS API will be able to autodetect: https://github.com/stoplightio/spectral/issues/369    
+    spectral.addFunctions(oas2Functions());
+    oas2Rules()
+      .then(rules => spectral.addRules(rules))
+      .then(() => {
+        // run!
+        spectral.run(content).then(results => {
+          context.github.issues.createComment(context.issue({ body: formatLintResults(results)}))
+        })
+      })
   })
 }
 
 const formatLintResults = lintResults => {
-    let output = '## You have ' + lintResults.length + ' specification lint warnings and errors:'
-    lintResults.forEach(result => {
-      var { path, name, type, summary, severity, message } = result;
-      path = path.join(" - ")      
-      output += `
-### ${path} \n **${name}** \n ${summary} \n 
-<details><summary>More details</summary>${message}</details>
-`
-    }) 
-  return output
+  let output = '## You have ' + lintResults.length + ' specification lint warnings and errors:\n'
+    Object.keys(lintResults).forEach(function (key){
+      var { path, code, severity, message, range} = lintResults[key]
+      output += `**${path.join(' : ')}**\n Line: ${range.start.line}\n **${code}**\n ${message}\n\n`
+     })  
+     return output
 }
